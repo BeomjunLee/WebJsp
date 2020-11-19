98ad1bd6e634e0b728b5c21cb160506af7551577
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2020-11-19 20:07:06 +0900

    수정

--- EventInfo/src/com/event/controller/FrontController.java
+++ EventInfo/src/com/event/controller/FrontController.java
@@ -1,13 +1,15 @@
 package com.event.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/FrontController")
+@WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     public FrontController() {
@@ -25,8 +27,30 @@ public class FrontController extends HttpServlet {
 		System.out.println("actionDo() 호출");
 		
 		request.setCharacterEncoding("UTF-8");
-
-		//안녕
-		//방가
+			
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		
+		String viewPage = null;  // 어떠한 페이지에 보여줄지 (View)
+		Command command = null;   // 어떠한 로직을 수행할지
+		
+		// 테스트 출력
+		System.out.println("uri: " + uri);
+		System.out.println("conPath: " + conPath);
+		System.out.println("com: " + com);
+		
+		switch(com) {
+		case "/":
+			
+			break;
+		}
+		
+		// response 를 위해서 
+		// 위에서 결정된 viewPage 에 forward 해줌
+		if(viewPage != null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+			dispatcher.forward(request, response);
+		}
 	}
 }
